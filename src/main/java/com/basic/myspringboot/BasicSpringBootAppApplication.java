a74e9b2a720b8e406b4b6ce7c8c66d8452220ddd
springboot <damhee6624@gmail.com>
2024-04-22 10:01:25 +0900

    환경 설정 커밋

---
+++ src/main/java/com/basic/myspringboot/BasicSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.basic.myspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringBootAppApplication.class, args);
+	}
+
+}
