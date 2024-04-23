08f470e0035404ac9d3885293426ec68265dd61d
springboot <damhee6624@gmail.com>
2024-04-23 15:33:56 +0900

    에러페이지 적용

---
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -0,0 +1,43 @@
+package com.basic.myspringboot.controller;
+
+import com.basic.myspringboot.entity.User;
+import com.basic.myspringboot.exception.BusinessException;
+import com.basic.myspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserRestController {
+    private final UserRepository userRepository;
+
+    //Constructor Injection
+//    public UserRestController(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    //@RequestMapping(value="/users", produces = { "application/json" })
+    @GetMapping
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public User getUser(@PathVariable Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        return optionalUser
+                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
+    }
+
+
+}
---
+++ src/main/java/com/basic/myspringboot/entity/User.java
@@ -0,0 +1,27 @@
+package com.basic.myspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@DynamicUpdate
+@Getter @Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+}
